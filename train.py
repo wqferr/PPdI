b3d01bd1f9870aa8f1b5f9e9dc8fda91dabd0106
wqferr <wqferr@gmail.com>
2017-05-16 22:58:15 -0300

    Upload buggy version for revision (YEAH I DID IT ON MASTER)

--- train.py
+++ train.py
@@ -3,6 +3,10 @@ from sys import argv
 import keras
 
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+
 
 # argv = ['train.py', 'class', rotation_range, width_shift_range, height_shift_range,
 #           rescale, shear_range, zoom_range]
@@ -12,26 +16,68 @@ argv = argv[1:]
 #           rescale, shear_range, zoom_range]
 
 # Default args
-arg = [45, 0.2, 0.2, 1./255, 0.2, 0.2]
+arg = [150, 45, 0.2, 0.2, 1./255, 0.2, 0.2]
 
-arg = [float(x) for x in argv[1:]] + arg[len(argv)-1:]
+arg = [float(x) for x in argv] + arg[len(argv):]
 
-datagen = ImageDataGenerator(
-        rotation_range=arg[0],
-        width_shift_range=arg[1],
-        height_shift_range=arg[2],
-        rescale=arg[3],
-        shear_range=arg[4],
-        zoom_range=arg[5],
+
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), input_shape=(3, arg[0], arg[0])))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), dim_ordering='tf'))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), dim_ordering='tf'))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), dim_ordering='tf'))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+        optimizer='rmsprop',
+        metrics=['accuracy'])
+
+batch_size = 16
+
+train_datagen = ImageDataGenerator(
+        rotation_range=arg[1],
+        width_shift_range=arg[2],
+        height_shift_range=arg[3],
+        rescale=arg[4],
+        shear_range=arg[5],
+        zoom_range=arg[6],
         horizontal_flip=True,
-        fill_mode='nearest')
+        fill_mode='nearest',
+        save_to_dir='out')
 
-img = load_img('data/{}/0.jpg'.format(argv[0]))
-x = img_to_array(img)
-x = x.reshape((1,) + x.shape)
+train_generator = train_datagen.flow_from_directory(
+        'data',
+        target_size=(arg[0], arg[0]),
+        batch_size=batch_size,
+        class_mode='binary')
 
-i = 0
-for batch in datagen.flow(x, batch_size=1, save_to_dir='out', save_prefix=argv[0], save_format='jpeg'):
-    i += 1
-    if i > 20:
-        break;
+test_datagen = ImageDataGenerator(
+        rescale=1./255,
+        save_to_dir='out')
+
+validation_generator = test_datagen.flow_from_directory(
+        'data',
+        target_size=(arg[0], arg[0]),
+        batch_size=batch_size,
+        class_mode='binary')
+
+model.fit_generator(
+        train_generator,
+        steps_per_epoch=2000 // batch_size,
+        epochs=50,
+        validation_data=validation_generator,
+        validation_steps=800 // batch_size)
