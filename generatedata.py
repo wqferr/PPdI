50dd5f62251cad51e8b506cb3cf74a9855c08575
eduardoscsouza <eduardoscsouza@gmail.com>
2017-06-16 13:09:48 -0300

    Minor fixes again

--- generatedata.py
+++ generatedata.py
@@ -1,13 +1,12 @@
 #Author: Eduardo Santos Carlos de Souza
 
 #Usage:
-#argv[1] = output image height
-#argv[2] = output image width
-#argv[3] = # of batches
-#argv[4] = path to load input images
-#argv[5] = path to save output arrays
-#argv[6] = prefix for the arrays' filenames
-#argv[7] = bool to apply random variations
+#argv[1] = path to load input images
+#argv[2] = output image height
+#argv[3] = output image width
+#argv[4] = path to save output arrays
+#argv[5] = bool to apply random variations
+#argv[6] = # of batches
 
 from keras.preprocessing.image import ImageDataGenerator
 from math import ceil
@@ -16,24 +15,21 @@ import time
 import sys
 import os.path
 
+in_path = os.path.join("..", "Data", "Datasets", "filtered", "train")
 out_shape = (224, 224)
-dataset_rep_count = 10
-in_path = os.path.join("..", "Data", "Datasets", "filtered")
-out_path = os.path.join("..", "Data", "Datasets", "keras")
-out_prefix = ""
+out_path = os.path.join("..", "Data", "Datasets", "keras") + "train"
 rand = True
-if (len(sys.argv) >= 3):
-	out_shape = (int(sys.argv[1]), int(sys.argv[2]))
+dataset_rep_count = 10
+if (len(sys.argv) >= 2):
+	in_path = sys.argv[1]
 if (len(sys.argv) >= 4):
-	dataset_rep_count = int(sys.argv[3])
+	out_shape = (int(sys.argv[2]), int(sys.argv[3]))
 if (len(sys.argv) >= 5):
-	in_path = sys.argv[4]
+	out_path = sys.argv[4]
 if (len(sys.argv) >= 6):
-	out_path = sys.argv[5]
+	rand = (sys.argv[5] == 'true')
 if (len(sys.argv) >= 7):
-	out_prefix = sys.argv[6] + "_"
-if (len(sys.argv) >= 8):
-	rand = (sys.argv[7] == 'true')
+	dataset_rep_count = int(sys.argv[6])
 
 batch_size = 256
 img_gen = ImageDataGenerator(fill_mode='constant', cval=0.0)
@@ -53,5 +49,5 @@ if (dataset_rep_count > 0):
 	print(x.shape)
 	print("Labels array: ", end="")
 	print(y.shape)
-	np.save(os.path.join(out_path, out_prefix + "images"), x)
-	np.save(os.path.join(out_path, out_prefix + "labels"), y)
+	np.save(out_path + "_images", x)
+	np.save(out_path + "_labels", y)
