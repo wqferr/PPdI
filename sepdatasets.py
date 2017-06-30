22a9c99c1ad89d42ac6dd7baa403936ddc3c33a7
wqferr <wqferr@gmail.com>
2017-06-30 00:05:42 -0300

    Improve usability of sepdatasets

--- sepdatasets.py
+++ sepdatasets.py
@@ -7,60 +7,57 @@ from shutil import copyfile as cp
 
 # get an argument from argv with a default value
 def get_arg(n, default):
-    if len(argv) > n:
-        return argv[n]
-    return default
+	if len(argv) > n:
+		return argv[n]
+	return default
 
 # execute cp command and add prefix if required
-def copy_img(lst, frm, to, rename):
-    for entry in lst:
-        name = os.path.basename(entry.name)
-        if rename:
-            new_name = os.path.join(frm, name)
-            new_name = new_name.replace(os.sep, '_')
-        else:
-            new_name = name
-        new_path = os.path.join(to, new_name)
-        cp(entry.path, new_path)
+def copy_img(lst, frm, to):
+	for entry in lst:
+		name = os.path.basename(entry.name)
+		new_path = os.path.join(to, name)
+		cp(entry.path, new_path)
 
-# Args:
-# 1 - The percent of images which will be used as the test dataset (default 20)
-# 2 - The source directory of the images (default '.')
-# 3 - The output root directory (default source directory)
-# 4 - The training dataset subdirectory (default 'train')
-# 5 - The test dataset subdirectory (default 'test')
-# 6 - Boolean value, true if should prepend origin directory to file copy (default false)
-if __name__ == '__main__':
-    test_percent = float(get_arg(1, 20)) / 100
-    src_dir = get_arg(2, '.')
-    out_dir = get_arg(3, src_dir)
-    train_subdir = get_arg(4, 'train')
-    test_subdir = get_arg(5, 'test')
-    rename = get_arg(6, 'false').strip().lower() == 'true'
-
-    train_dir = os.path.join(out_dir, train_subdir)
-    test_dir = os.path.join(out_dir, test_subdir)
-
-	# create output directory if it doesn't exist
-    os.makedirs(train_dir, 0o777, True)
-    os.makedirs(test_dir, 0o777, True)
-
-	# list all images in input directory
-    images = []
-    entries = os.scandir(src_dir)
-    for entry in entries:
-        if entry.is_file():
-            images.append(entry)
-    del entries
-
+def copy_dataset(src_dir_entry, out_dir):
+	dataset = src_dir_entry.name
+	src_dir = src_dir_entry.path
+	test_dir = os.path.join(out_dir, 'test', dataset)
+	train_dir = os.path.join(out_dir, 'train', dataset)
+	
+	os.makedirs(train_dir, 0o777, True)
+	os.makedirs(test_dir, 0o777, True)
+	
+	# find every file in the subdirectory
+	images = []
+	entries = os.scandir(src_dir)
+	for entry in entries:
+		if entry.is_file():
+			images.append(entry)
+	
 	# order them randomly
-    shuffle(images)
+	shuffle(images)
 	
 	# n_test first will be part of the test set, and the remaining will be used for training
-    n_test = math.ceil(len(images) * test_percent)
-    train_images = images[n_test:]
-    test_images = images[:n_test]
-    
+	n_test = math.ceil(len(images) * test_percent)
+	train_images = images[n_test:]
+	test_images = images[:n_test]
+	
 	# copy images to test and train directories
-    copy_img(train_images, src_dir, train_dir, rename)
-    copy_img(test_images, src_dir, test_dir, rename)
+	copy_img(train_images, src_dir, train_dir)
+	copy_img(test_images, src_dir, test_dir)
+
+	
+# Args:
+# 1 - The percent of images which will be used as the test dataset (default 20)
+# 2 - The source directory of the images (default '../Data/Datasets/filtered')
+# 3 - The output root directory (default ../Data/Datasets/separated)
+if __name__ == '__main__':
+	test_percent = float(get_arg(1, 20)) / 100
+	src_dir = get_arg(2, os.path.join('..', 'Data', 'Datasets', 'filtered'))
+	out_dir = get_arg(3, os.path.join('..', 'Data', 'Datasets', 'separated'))
+
+	# treat every subdirectory of src_dir as a class and copy it to separated/
+	entries = os.scandir(src_dir)
+	for entry in entries:
+		if entry.is_dir():
+			copy_dataset(entry, out_dir)
